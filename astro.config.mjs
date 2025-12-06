19a422ae4fb509f2eebed6545c033defd905b558
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2025-12-06 11:15:10 -0500

    Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  site: 'https://djkhaled.github.io/astro-workshop/'
+  site: 'https://github.com/LizethVictoria20/astro'
 });
 
